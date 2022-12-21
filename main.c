d6a4cb9ab2c7c7c313f70fd3f100a06cd4c343b1
mtcnbzks <mtcnbzks@gmail.com>
2022-12-21 17:26:52 +0300

    added command line arguments and testing func section

--- main.c
+++ main.c
@@ -11,9 +11,13 @@
 
 #include "matrixLib.h" // for function prototypes
 
-int main() {
+int main(int argc, char *argv[]) {
+
+    int seed = argv[1] ? atoi(argv[1]) : 0; // get the seed from command line
+
     int row = 3, col = 3;
     float **mat = returnMatrix(row, col);
+    // fillMatrix(mat, row, col, seed);
 
     mat[0][0] = 3;
     mat[0][1] = 2;
@@ -21,13 +25,15 @@ int main() {
 
     mat[1][0] = 4;
     mat[1][1] = 5;
-    mat[1][2] = 2;
+    mat[1][2] = 6;
 
-    mat[2][0] = 2;
-    mat[2][1] = 4;
-    mat[2][2] = 3;
+    mat[2][0] = 7;
+    mat[2][1] = 8;
+    mat[2][2] = 9;
+
 
     float *vec = returnVector(row);
+    // fillVector(vec, row, seed);
     float *vec2 = returnVector(col);
 
     // fill with 1,2,3
@@ -44,36 +50,34 @@ int main() {
     printMatrix(mat, row, col);
     printf("------------------------\n");
 
-
-
-//    float *rMeans = rowMeans(mat, row, col);
-//    float *cMeans = columnMeans(mat, row, col);
-//    printf("Row means: \n");
-//    printVector(rMeans, row);
-//    printf("Column means: \n");
-//    printVector(cMeans, col);
-
-//    float **transpose = matrixTranspose(mat, row, col);
-//    printf("Transpose: \n");
-//    printMatrix(transpose, col, row);
-
-//    float corr = correlation(vec, vec2, row);
-//    printf("Correlation: %f\n", corr);
-
-//    float **covMat = covarianceMatrix(mat, row, col);
-//    printf("Covariance matrix: \n");
-//    printMatrix(covMat, col, col);
-
-//    float cov = covariance(vec, vec2, row);
-//    printf("Covariance: %f\n", cov);
-
-
-//    float **mat3 = matrixMultiplication(mat, transpose, row, col, col, row);
-//    printf("Matrix multiplication: \n");
-//    printMatrix(mat3, row, col);
-
     float det = determinant(mat, row);
     printf("Determinant: %f\n", det);
+
+
+    float **transpose = matrixTranspose(mat, row, col);
+    printf("Transpose: \n");
+    printMatrix(transpose, col, row);
+
+    float **mat3 = matrixMultiplication(mat, transpose, row, col, col, row);
+    printf("Matrix multiplication: \n");
+    printMatrix(mat3, row, col);
+
+    float *rMeans = rowMeans(mat, row, col);
+    float *cMeans = columnMeans(mat, row, col);
+    printf("Row means: \n");
+    printVector(rMeans, row);
+    printf("Column means: \n");
+    printVector(cMeans, col);
+
+    float cov = covariance(vec, vec2, row);
+    printf("Covariance: %f\n", cov);
+
+    float corr = correlation(vec, vec2, row);
+    printf("Correlation: %f\n", corr);
+
+    float **covMat = covarianceMatrix(mat, row, col);
+    printf("Covariance matrix: \n");
+    printMatrix(covMat, row, row);
 
     freeMatrix(mat, row);
     free(vec);
