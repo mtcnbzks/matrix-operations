33bebf48b617284d1eb729df018bf5c3b0165a3e
mtcnbzks <mtcnbzks@gmail.com>
2023-01-12 21:15:00 +0300

    file name change

--- main.c
+++ main.c
@@ -1,17 +1,18 @@
 /**
- * @file main.c
+ * @file Main.c
  * @description Testing my functions
  * @assignment Project 1
  * @date 28.11.2022
  * @author Mehmetcan Bozkuş - mehmetcan.bozkus@stu.fsm.edu.tr
  */
 
-#include <stdio.h> // for printf
+#include <stdio.h>  // for printf
 #include <stdlib.h> // for malloc and free
 
 #include "matrixLib.h" // for function prototypes
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
 
     int seed = argv[1] ? atoi(argv[1]) : 0; // get the seed from command line
 
@@ -31,21 +32,19 @@ int main(int argc, char *argv[]) {
     mat[2][1] = 8;
     mat[2][2] = 9;
 
-
-    float *vec = returnVector(4);
+    float *vec = returnVector(row);
     // fillVector(vec, row, seed);
-    float *vec2 = returnVector(4);
+    float *vec2 = returnVector(row);
 
     // fill with 1,2,3
     vec[0] = 4;
     vec[1] = 3;
     vec[2] = 2;
-    vec[3] = 9;
+
     // fill with 1,2,3
     vec2[0] = 5;
     vec2[1] = 5;
     vec2[2] = 8;
-    vec2[3] = 3;
 
     // printMatrix(mat2, row2, col2);
     printMatrix(mat, row, col);
@@ -53,7 +52,6 @@ int main(int argc, char *argv[]) {
 
     float det = determinant(mat, row);
     printf("Determinant: %f\n", det);
-
 
     float **transpose = matrixTranspose(mat, row, col);
     printf("Transpose: \n");
